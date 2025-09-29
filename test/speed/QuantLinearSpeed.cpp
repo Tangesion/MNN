7ca636d265a0aed6558dc6f1cb04d60a549e6dff
tangesion <tangesion@163.com>
2025-09-29 15:28:10 +0800

    update

--- test/speed/QuantLinearSpeed.cpp
+++ test/speed/QuantLinearSpeed.cpp
@@ -1,21 +1,13 @@
-#include <cstdint>
+
 #include <math.h>
 #include <MNN/expr/ExprCreator.hpp>
-#include "MNN/expr/Expr.hpp"
-#include "MNN/expr/NeuralNetWorkOp.hpp"
 #include "MNNTestSuite.h"
 #include <MNN/AutoTime.hpp>
 #include <MNN/Interpreter.hpp>
-#include "core/Session.hpp"
-#include <regex>
-#include <thread>
-#include <vector>
-#include "MNN_generated.h"
+#include "CommonOpCreator.hpp"
 
 using namespace MNN::Express;
 using namespace MNN;
-
-
 
 class LinearSpeedTest : public MNNTestCase {
 public:
@@ -28,68 +20,139 @@ public:
         // Asymmetric, Block-wise
         res = res && testLinear("Linear Asymm Block64", 1, 1024, 1024, 8, true, 64);
         // 4-bit version
-        res = res && testLinear("Linear Asymm Block64 4bit", 1, 1024, 1024, 4, true, 64);
+        res = res &&
+            testLinear("Linear Asymm Block64 4bit", 1, 1024, 1024, 4, true, 64);
         return res;
-    }
+  }
 
 private:
-    bool testLinear(std::string title, int batch, int ic, int oc, int nbit = 8, bool asymmetric = false, int blockSize = 0) {
-        std::vector<int> bias(oc);
-        int numBlocks = (blockSize > 0) ? ic / blockSize : 1;
-        int scaleSize = asymmetric ? oc * numBlocks * 2 : oc * numBlocks;
-        std::vector<int> channel = {ic, oc};
+    bool testLinear(std::string title, int batch, int ic, int oc, int nbit = 8,
+                  bool asymmetric = false, int blockSize = 0) {
         std::vector<int> kernel = {1, 1};
-        std::vector<int> strides = {1, 1}, dilate = {1, 1}, pad = {1, 1};
-        std::vector<float> scale(scaleSize);
-        std::vector<int8_t> weight(oc * ic);
-        VARP x = _Input({batch, ic, 1, 1}, MNN::Express::NC4HW4, halide_type_of<int8_t>());
+        std::vector<int> pad = {0, 0};
+        std::vector<int> channel = {ic, oc};
+        std::vector<int> stride = {1, 1};
+        std::vector<int> dilate = {1, 1};
 
+        float fac = 0.23;
+        int res = 10;
+        float tail = 0.05;
+        int iw = 1, ih = 1;
+        std::vector<float> bias(oc), biastest(oc), biasdup(oc);
+        int blockNum = 1;
+        if (0 == blockSize || ic % blockSize != 0) {
+            blockSize = ic;
+            blockNum = 1;
+        } else {
+            blockNum = ic / blockSize;
+        }
+
+        std::vector<float> weightFp32(oc * ic);
+
+        std::vector<float> wScale;
+        if (asymmetric) {
+            wScale.resize(2 * oc * blockNum);
+        } else {
+            wScale.resize(oc * blockNum);
+        }
+
+        float threshold = (float)(1 << (nbit - 1)) - 1.0f;
+        float clampMin = -threshold - 1;
+        VARP x = _Input({batch, ic, ih, iw}, NCHW, halide_type_of<float>());
         auto xInfo = x->getInfo();
-        auto xPtr = x->writeMap<int8_t>();
-        int8_t xMin = -(1<<(nbit-1))+1, xMax = (1<<(nbit-1))-1;
-
+        auto xPtr = x->writeMap<float>();
+        int8_t xMin = -(1 << (nbit - 1)), xMax = (1 << (nbit - 1)) - 1;
+        for (int i = 0; i < xInfo->size; ++i) {
+            xPtr[i] = (i % (xMax - xMin + 1) - (xMax / 2)) * 0.017;
+        }
+        x = _Convert(x, NC4HW4);
         for (int i = 0; i < oc; ++i) {
-            bias[i] = (10000 + i*i*10 - i*i*i) % 12580;
+            bias[i] = i % 10 + 0.005;
             for (int j = 0; j < ic; ++j) {
-                weight[i * ic + j] = (i * 13 + j * 37 + 1234) % (xMax - xMin + 1) + xMin;
+                weightFp32[i * ic + j] = (i * ic + j) % res * fac + tail;
+            }
+        }
+        ::memcpy(biastest.data(), bias.data(), oc * sizeof(float));
+        ::memcpy(biasdup.data(), bias.data(), oc * sizeof(float));
+        int kernel_size = ic;
+        auto newWeightFp32 = weightFp32;
+
+        for (int k = 0; k < oc; ++k) {
+            for (int j = 0; j < blockNum; ++j) {
+                int index = k * blockNum + j;
+                auto minmax = findMinMax(weightFp32.data() + k * ic + j * blockSize, blockSize);
+                if (asymmetric) {
+                    auto scale_ = (minmax.second - minmax.first) / (threshold - clampMin);
+                    wScale[2 * index] = minmax.first;
+                    wScale[2 * index + 1] = scale_;
+                    for (int u = 0; u < blockSize; ++u) {
+                        int idx = k * ic + j * blockSize + u;
+                        int q_weight = (weightFp32[idx] - minmax.first) * (threshold - clampMin) / (minmax.second - minmax.first) + clampMin;
+                        newWeightFp32[idx] = (q_weight - xMin) * scale_ + minmax.first;
+                    }
+                } else {
+                    float absMax = std::max(std::abs(minmax.first), std::abs(minmax.second));
+                    auto scale_ = absMax / threshold;
+                    wScale[index] = scale_;
+                    for (int u = 0; u < blockSize; ++u) {
+                        int idx = k * ic + j * blockSize + u;
+                        int q_weight = weightFp32[idx] / scale_;
+                        newWeightFp32[idx] = q_weight * scale_;
+                    }
+                }
             }
         }
 
-        for (int i = 0; i < int(scaleSize / 2); ++i) {
-            scale[i * 2] =  2;
-            scale[i * 2 + 1] = fabs(((127-i)*i % 128) / 20000.0f);
-        }
-        x = _FloatToInt8(_Cast<float>(x), _Scalar<float>(1.0f), -127, 127);
-        auto y     = _Conv(std::vector<int8_t>(weight), std::vector<int>(bias), std::vector<float>(scale), x,
-                           channel, kernel, PaddingMode::CAFFE, strides, dilate, 1, pad, false, 0, 0, -127, 127, false);
-
-        if (nbit != 8) {
-            std::unique_ptr<MNN::OpT> op(y->expr().first->get()->UnPack());
-            op->main.AsConvolution2D()->symmetricQuan->nbits = nbit;
-            y = Variable::create(Expr::create(op.get(), {x}));
-            op.reset();
-        }
-        auto yr = _Int8ToFloat(y, _Scalar<float>(1.0f));
-        yr = _Cast<int8_t>(yr);
+        auto y     = _HybridConv(weightFp32, std::move(bias), std::move(wScale), x, channel, kernel, PaddingMode::CAFFE, stride, dilate, 1, pad, false, false, nbit, asymmetric);
+        auto yfp32 = _Conv(std::move(newWeightFp32), std::move(biasdup), x, {ic, oc}, kernel, PaddingMode::CAFFE, stride, dilate, 1, pad);
         auto yInfo = y->getInfo();
-        auto yPtr  = yr->readMap<int8_t>();
         auto ow = yInfo->dim[3], oh = yInfo->dim[2];
-
-        {
-            x.fix(VARP::INPUT);
-            MNN::Timer _t;
-            const int LOOP = 20;
-            for (int i = 0; i < LOOP; ++i) {
-                x->writeMap<float>();
-                y->readMap<float>();
-            }
-            auto time = (float)_t.durationInUs() / 1000.0f;
-            MNN_PRINT("%s kernel=(%dx%d) input=(1x%dx%dx%d) output=(1x%dx%dx%d) stride=(%dx%d), avg time = %f\n",
-                      title.c_str(), 1, 1, ic, 1, 1, oc, oh, ow, strides[1], strides[0], 1.0 * time / LOOP);
+#if defined (__aarch64__) && (precision == 2)
+#define FLOAT_T __fp16
+#else
+#define FLOAT_T float
+#endif
+        y = _Convert(y, NCHW);
+        yfp32 = _Convert(yfp32, NCHW);
+        auto yPtr  = y->readMap<FLOAT_T>();
+        auto tgPtr = yfp32->readMap<FLOAT_T>();
+        auto elesize = yfp32->getInfo()->size;
+        float limit = 0.1f;
+        bool correct = true;
+        float maxValue = 0.001f;
+        for (int i = 0; i < elesize; ++i) {
+            maxValue = fmaxf(maxValue, fabsf(tgPtr[i]));
         }
 
-        return true;
+        for (int i = 0; i < elesize; ++i) {
+            float targetValue = tgPtr[i], computeResult = yPtr[i];
+            float diff = targetValue - computeResult;
+            float ratio = fabsf(diff) / maxValue;
+            if (ratio > limit) {
+                MNN_PRINT("%d result Error ratio=%f: right=%f, error=%f\n", i, ratio, targetValue, computeResult);
+                MNN_PRINT("conv info: input=(%dx%dx%dx%d) output=(%dx%dx%dx%d)\n", batch, ic, ih, iw, batch, oc, oh, ow);
+                correct = false;
+                break;
+            }
+        }
+        x.fix(VARP::INPUT);
+        const int LOOP = 100;
+        {
+            x->writeMap<FLOAT_T>();
+            y->readMap<FLOAT_T>();
+        }
+        MNN::Timer _t;
+        for (int i = 0; i < LOOP; ++i) {
+            x->writeMap<FLOAT_T>();
+            y->readMap<FLOAT_T>();
+        }
+        auto time = (float)_t.durationInUs() / 1000.0f;
+        MNN_PRINT("%s input=(%dx%dx%dx%d) output=(%dx%dx%dx%d) avg time = %f\n",
+                    title.c_str(), batch, ic, ih, iw, batch, oc, oh, ow, 1.0 * time / LOOP);
+        
+        return correct;
 
+        
     }
 };
 
